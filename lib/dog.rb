f9205e2311d2fd23010f7d07b6aacfe5d622ab9c
Taylor Treece <taylor.treece@protonmail.com>
2021-01-15 10:38:06 -0600

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,32 @@
-# Add your code here
+require 'pry'
+
+class Dog
+
+    @@all = []
+    @@names = []
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+        @@names << name
+        save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        @@all = []
+    end
+
+    def self.print_all
+        @@names.delete("Snoopy")
+        puts @@names.uniq
+    end
+
+    def save
+        @@all << self
+    end
+end
